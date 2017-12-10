36d3ee753085411f0e42a61cda6756ce66efd7a6
Adam Gall <adam@allgall.net>
2017-12-10 09:42:30 -0500

    Stops ignoring admin and manage

---
+++ ToDo/admin.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+from ToDo.models import TaskList, Task
+
+
+class TaskInline(admin.TabularInline):
+    model = Task
+    extra = 3
+
+
+class TaskListAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,	{'fields': ['owner', 'status', 'title', 'left', 'top', 'width', 'height']})
+    ]
+    inlines = [TaskInline]
+
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'status', 'created_date', 'completed_date', 'sortOrder')
+    list_filter = ['status', 'created_date']
+    search_fields = ['title']
+
+
+admin.site.register(TaskList, TaskListAdmin)
+admin.site.register(Task, TaskAdmin)
