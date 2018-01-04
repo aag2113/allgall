45884ba41a9190b59551dde84c0d1c1b70eedb0d
Adam Gall <adam@allgall.net>
2018-01-03 21:03:38 -0500

    Update Spots

--- adam/management/commands/spots.py
+++ adam/management/commands/spots.py
@@ -1,21 +1,32 @@
 import spotipy
+import spotipy.util as sputil
 
 from django.core.management.base import BaseCommand
 from django.conf import settings
 
 
 class Command(BaseCommand):
-	def handle(self, *args, **options):
-		PLAYLIST_ID_100 = settings.PLAYLIST_ID_100
-		SPOTIFY_O_AUTH_TOKEN = settings.SPOTIFY_O_AUTH_TOKEN
+    def handle(self, *args, **options):
+        user = settings.SPOTIFY.get('user')
+        scope = settings.SPOTIFY.get('scope')
+        client_id = settings.SPOTIFY.get('client_id')
+        client_secret = settings.SPOTIFY.get('client_secret')
+        redirect_uri = settings.SPOTIFY.get('redirect_uri')
+        playlist_id = settings.SPOTIFY.get('playlist_id')
 
-		try:
-			sp = spotipy.Spotify(auth=SPOTIFY_O_AUTH_TOKEN)
-		except:
-			print('OAuth token expired')
-		tracks_1 = sp.current_user_saved_tracks(limit=50)
-		tracks_2 = sp.current_user_saved_tracks(limit=50, offset=50)
+        token = sputil.prompt_for_user_token(
+            user,
+            scope,
+            client_id=client_id,
+            client_secret=client_secret,
+            redirect_uri=redirect_uri
+        )
 
-		track_ids = [track.get('track').get('id') for track in tracks_1.get('items')]
-		track_ids.extend([track.get('track').get('id') for track in tracks_2.get('items')])
-		sp.user_playlist_replace_tracks("adam.gall", PLAYLIST_ID_100, track_ids)
+        sp = spotipy.Spotify(auth=token)
+
+        tracks_1 = sp.current_user_saved_tracks(limit=50)
+        tracks_2 = sp.current_user_saved_tracks(limit=50, offset=50)
+
+        track_ids = [track.get('track').get('id') for track in tracks_1.get('items')]
+        track_ids.extend([track.get('track').get('id') for track in tracks_2.get('items')])
+        sp.user_playlist_replace_tracks(user, playlist_id, track_ids)
